18bbda0242d62e053c1db59a9d394436b94b839c
Валентин <admin@creator-spb.ru>
2022-09-18 17:13:40 +0300

    feat: add solutions colorBrgightest.ts

--- 6-kyu/createPhoneNumber.js
+++ 6-kyu/createPhoneNumber.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 * Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string
 of those numbers in the form of a phone number.
@@ -8,9 +9,6 @@ of those numbers in the form of a phone number.
 * The returned format must be correct in order to complete this challenge.
 * Don't forget the space after the closing parentheses!
 */
-
-const createPhoneNumber = (numbers: number[]) => numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
-
-
+const createPhoneNumber = (numbers) => numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
 // Test cases
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // (123) 456-7890
